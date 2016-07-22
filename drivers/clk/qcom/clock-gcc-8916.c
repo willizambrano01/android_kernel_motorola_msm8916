93462fc63237642c9cf5c2110f76e8534ccbe581
CarlosArriagaCM <CarlosArriagaCM@gmail.com>
2016-07-21 19:56:55 -0500

    OC CPU and GPU

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -56,7 +56,6 @@ static void __iomem *virt_bases[N_BASES];
 #define GPLL1_USER_CTL					0x20010
 #define GPLL1_CONFIG_CTL				0x20014
 #define GPLL1_STATUS					0x2001C
-#define SNOC_QOSGEN					0x2601C
 #define GPLL2_MODE					0x4A000
 #define GPLL2_L_VAL					0x4A004
 #define GPLL2_M_VAL					0x4A008
@@ -348,6 +347,11 @@ static struct pll_freq_tbl apcs_pll_freq[] = {
 	F_APCS_PLL(1248000000, 65, 0x0, 0x1, 0x0, 0x0, 0x0),
 	F_APCS_PLL(1363200000, 71, 0x0, 0x1, 0x0, 0x0, 0x0),
 	F_APCS_PLL(1401600000, 73, 0x0, 0x1, 0x0, 0x0, 0x0),
+	F_APCS_PLL(1497600000, 78, 0x0, 0x1, 0x0, 0x0, 0x0),
+	F_APCS_PLL(1593600000, 83, 0x0, 0x1, 0x0, 0x0, 0x0),
+	F_APCS_PLL(1689600000, 88, 0x0, 0x1, 0x0, 0x0, 0x0),
+	F_APCS_PLL(1785600000, 93, 0x0, 0x1, 0x0, 0x0, 0x0),
+	F_APCS_PLL(1881600000, 98, 0x0, 0x1, 0x0, 0x0, 0x0),
 	PLL_F_END
 };
 
@@ -551,7 +555,10 @@ static struct clk_freq_tbl ftbl_gcc_camss_vfe0_clk[] = {
 	F( 266670000,	   gpll0,   3,	  0,	0),
 	F( 320000000,	   gpll0, 2.5,	  0,	0),
 	F( 400000000,	   gpll0,   2,	  0,	0),
-	F( 465000000,	   gpll2,   2,	  0,	0),
+	F( 475000000,      gpll2,   2,	  0,	0),
+	F( 550000000,      gpll2,   2,	  0,	0),
+	F( 650000000,      gpll2,   2,	  0,	0),
+	F( 720000000,      gpll2,   2,    0,    0),
 	F_END
 };
 
@@ -565,12 +572,12 @@ static struct rcg_clk vfe0_clk_src = {
 		.dbg_name = "vfe0_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 160000000, NOMINAL, 320000000, HIGH,
-			465000000),
+			720000000),
 		CLK_INIT(vfe0_clk_src.c),
 	},
 };
 
-static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
+static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_720_clk[] = {
 	F(  19200000,	      xo,   1,	  0,	0),
 	F(  50000000,  gpll0_aux,  16,	  0,	0),
 	F(  80000000,  gpll0_aux,  10,	  0,	0),
@@ -585,6 +592,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
 	F( 475000000,      gpll2,   2,	  0,	0),
 	F( 550000000,      gpll2,   2,	  0,	0),
 	F( 650000000,      gpll2,   2,	  0,	0),
+	F( 720000000,      gpll2,   2,    0,    0),
 	F_END
 };
 
@@ -603,6 +611,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 475000000,      gpll2,   2,	  0,	0),
 	F( 550000000,      gpll2,   2,	  0,	0),
 	F( 650000000,      gpll2,   2,	  0,	0),
+	F( 720000000,      gpll2,   2,    0,    0),
 	F_END
 };
 
@@ -615,8 +624,8 @@ static struct rcg_clk gfx3d_clk_src = {
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP3(LOW, 19200000, NOMINAL, 310000000, HIGH,
-			650000000),
+		VDD_DIG_FMAX_MAP3(LOW, 100000000, NOMINAL, 310000000, HIGH,
+			720000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
@@ -1000,7 +1009,7 @@ static struct rcg_clk csi1phytimer_clk_src = {
 static struct clk_freq_tbl ftbl_gcc_camss_cpp_clk[] = {
 	F( 160000000,	   gpll0,   5,	  0,	0),
 	F( 320000000,	   gpll0, 2.5,	  0,	0),
-	F( 465000000,	   gpll2,   2,	  0,	0),
+	F( 720000000,	   gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -1014,7 +1023,7 @@ static struct rcg_clk cpp_clk_src = {
 		.dbg_name = "cpp_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 160000000, NOMINAL, 320000000, HIGH,
-			465000000),
+			720000000),
 		CLK_INIT(cpp_clk_src.c),
 	},
 };
@@ -2362,18 +2371,6 @@ static struct branch_clk gcc_venus0_vcodec0_clk = {
 	},
 };
 
-static struct gate_clk gcc_snoc_qosgen_clk = {
-	.en_mask = BIT(0),
-	.en_reg = SNOC_QOSGEN,
-	.base = &virt_bases[GCC_BASE],
-	.c = {
-		.dbg_name = "gcc_snoc_qosgen_clk",
-		.ops = &clk_ops_gate,
-		.flags = CLKFLAG_SKIP_HANDOFF,
-		CLK_INIT(gcc_snoc_qosgen_clk.c),
-	},
-};
-
 static struct mux_clk gcc_debug_mux;
 static struct clk_ops clk_ops_debug_mux;
 
@@ -2762,9 +2759,6 @@ static struct clk_lookup msm_clocks_lookup[] = {
 	CLK_LIST(gcc_crypto_ahb_clk),
 	CLK_LIST(gcc_crypto_axi_clk),
 	CLK_LIST(crypto_clk_src),
-
-	/* QoS Reference clock */
-	CLK_LIST(gcc_snoc_qosgen_clk),
 };
 
 #define EFUSE_BASE	0x0005c004
@@ -2803,8 +2797,8 @@ static void gcc_gfx3d_fmax(struct platform_device *pdev)
 	pr_info("%s, Version: %d, bin: %d\n", __func__, version,
 					bin);
 
-	gfx3d_clk_src.c.fmax[VDD_DIG_HIGH] = 465000000;
-	gfx3d_clk_src.freq_tbl = ftbl_gcc_oxili_gfx3d_465_clk;
+	gfx3d_clk_src.c.fmax[VDD_DIG_HIGH] = 720000000;
+	gfx3d_clk_src.freq_tbl = ftbl_gcc_oxili_gfx3d_720_clk;
 }
 
 static int msm_gcc_probe(struct platform_device *pdev)
@@ -3055,3 +3049,4 @@ static int __init msm_gcc_mdss_init(void)
 	return platform_driver_register(&msm_clock_gcc_mdss_driver);
 }
 fs_initcall_sync(msm_gcc_mdss_init);
+
